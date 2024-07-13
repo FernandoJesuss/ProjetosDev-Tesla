00ef04165e7707751de9b3faf581c056b3c748c4
fernandojesuss <fernando_ffernando@hotmail.com>
2024-07-12 23:43:29 -0300

    Fix Alteração no codigo

---
+++ formulario.js
@@ -0,0 +1,28 @@
+// Váriaveis
+// Uma forma de Identificar quem é quem
+// Da um nome pra um pedacinho de código
+// É como se vc criasse uma pasta, para guardar o quiser
+
+// Funções
+// É um trecho de código que só executa, quando é chamado
+// document = ir no HTML
+// querySelector = busca o que eu quiser que ele busque
+
+
+
+
+
+
+
+let formulario = document.querySelector(".formulario")
+
+function aparecerformulario(){
+
+    formulario.style.left = "50%"
+
+}
+
+function desaparecerformulario(){
+formulario.style.left = "-50%"
+
+}
